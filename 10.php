a647a8bcf99e5919d3738f4c17e3c1e0ededc18a
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2023-09-13 16:41:32 +0500

    Add files via upload

---
+++ 10.php
@@ -0,0 +1,22 @@
+<?php  
+
+// string operator :
+////first example
+$a="HELLO";
+$b=  $a.  "RAO ALI MURAD";   /////. means concadenate
+ echo $b."<br>";
+
+
+//////////2nd example
+$a="HELLO";
+$a.=" rao";
+$a.=" ali";
+$a.=" murad";
+
+echo $a;
+
+
+
+
+
+?>
